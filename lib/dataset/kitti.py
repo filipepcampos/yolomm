92110002cbe5c65b9ca7be394c223e5e8688c9e6
Francisco Cerqueira <up201905337@edu.fe.up.pt>
2023-03-20 19:00:03 +0000

    feat: add base code to train with Kitti dataset

--- lib/dataset/kitti.py
+++ lib/dataset/kitti.py
@@ -15,6 +15,7 @@ from torch.utils.data import Dataset
 from ..utils import letterbox, augment_hsv, random_perspective, xyxy2xywh, cutout
 from lib.config import cfg
 
+single_cls = True  # just detect vehicle
 
 class KITTIDataset(Dataset):
     def __init__(self, cfg, is_train, inputsize=640, transform=None):
@@ -47,7 +48,8 @@ class KITTIDataset(Dataset):
         self.mask_root = mask_root / indicator / "image_2" # TODO: png files
         # self.lane_root = lane_root / indicator # TODO: Figure out how this works in KITTI?
         # self.label_list = self.label_root.iterdir()
-        self.mask_list = self.mask_root.iterdir()
+        #self.mask_list = self.mask_root.iterdir()
+        self.img_list = self.img_root.iterdir()
 
         self.db = []
 
@@ -60,6 +62,8 @@ class KITTIDataset(Dataset):
 
         # self.target_type = cfg.MODEL.TARGET_TYPE
         self.shapes = np.array(cfg.DATASET.ORG_IMG_SIZE)
+
+        self.db = self._get_db()
 
     def _get_db(self):
         """
@@ -77,43 +81,39 @@ class KITTIDataset(Dataset):
         print("building database...")
         gt_db = []
         height, width = self.shapes
-        for mask in tqdm(list(self.mask_list)):
-            mask_path = str(mask)
-            print(mask_path)
-            # label_path = mask_path.replace(
-            #     str(self.mask_root), str(self.label_root)
-            # ).replace(".png", ".json")
-            # image_path = mask_path.replace(
-            #     str(self.mask_root), str(self.img_root)
-            # ).replace(".png", ".jpg")
-            # lane_path = mask_path.replace(str(self.mask_root), str(self.lane_root))
-            # with open(label_path, "r") as f:
-            #     label = json.load(f)
-            # data = label["frames"][0]["objects"]
-            # data = self.filter_data(data)
-            # gt = np.zeros((len(data), 5))
-            # for idx, obj in enumerate(data):
-            #     category = obj["category"]
-            #     if category == "traffic light":
-            #         color = obj["attributes"]["trafficLightColor"]
-            #         category = "tl_" + color
-            #     if category in id_dict.keys():
-            #         x1 = float(obj["box2d"]["x1"])
-            #         y1 = float(obj["box2d"]["y1"])
-            #         x2 = float(obj["box2d"]["x2"])
-            #         y2 = float(obj["box2d"]["y2"])
-            #         cls_id = id_dict[category]
-            #         if single_cls:
-            #             cls_id = 0
-            #         gt[idx][0] = cls_id
-            #         box = convert((width, height), (x1, x2, y1, y2))
-            #         gt[idx][1:] = list(box)
 
-            # rec = [
-            #     {"image": image_path, "label": gt, "mask": mask_path, "lane": lane_path}
-            # ]
+        for image_path in tqdm(list(self.img_list)):
+            label_path = str(image_path).replace(
+                str(self.img_root), str(self.label_root)
+            ).replace(f".{self.data_format}", ".txt")
+            
+            lines = [line.split() for line in open(label_path).readlines()]
 
-            # gt_db += rec
+            gt = np.zeros((len(lines), 5))
+
+            for idx, line in enumerate(lines):
+                cls_id = 0 if single_cls else int(line[0])
+
+                left = float(line[4])
+                top = float(line[5])
+                right = float(line[6])
+                bottom = float(line[7])
+
+                center_x = (left + right) / 2
+                center_y = (top + bottom) / 2
+                w = right - left
+                h = bottom - top
+
+                bbox = [center_x // width, center_y // height, w // width, h // height]
+                gt[idx][0] = cls_id
+                gt[idx][1:] = bbox
+
+            rec = [
+                {"image": str(image_path), "label": gt}
+            ]
+
+            gt_db += rec
+
         print("database build finish")
         return gt_db
 
@@ -131,7 +131,7 @@ class KITTIDataset(Dataset):
         """
         return len(self.db)
 
-    def __getitem__(self, idx):
+    def _old__getitem__(self, idx):
         """
         Get input and groud-truth from database & add data augmentation on input
 
@@ -155,11 +155,11 @@ class KITTIDataset(Dataset):
         )
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # seg_label = cv2.imread(data["mask"], 0)
-        if self.cfg.num_seg_class == 3:
-            seg_label = cv2.imread(data["mask"])
-        else:
-            seg_label = cv2.imread(data["mask"], 0)
-        lane_label = cv2.imread(data["lane"], 0)
+        # if self.cfg.num_seg_class == 3:
+        #     seg_label = cv2.imread(data["mask"])
+        # else:
+        #     seg_label = cv2.imread(data["mask"], 0)
+        # lane_label = cv2.imread(data["lane"], 0)
         # print(lane_label.shape)
         # print(seg_label.shape)
         # print(lane_label.shape)
@@ -241,8 +241,8 @@ class KITTIDataset(Dataset):
             lr_flip = True
             if lr_flip and random.random() < 0.5:
                 img = np.fliplr(img)
-                seg_label = np.fliplr(seg_label)
-                lane_label = np.fliplr(lane_label)
+                # seg_label = np.fliplr(seg_label)
+                # lane_label = np.fliplr(lane_label)
                 if len(labels):
                     labels[:, 1] = 1 - labels[:, 1]
 
@@ -250,8 +250,8 @@ class KITTIDataset(Dataset):
             ud_flip = False
             if ud_flip and random.random() < 0.5:
                 img = np.flipud(img)
-                seg_label = np.filpud(seg_label)
-                lane_label = np.filpud(lane_label)
+                # seg_label = np.filpud(seg_label)
+                # lane_label = np.filpud(lane_label)
                 if len(labels):
                     labels[:, 2] = 1 - labels[:, 2]
 
@@ -275,41 +275,121 @@ class KITTIDataset(Dataset):
         # if idx == 0:
         #     print(seg_label[:,:,0])
 
-        if self.cfg.num_seg_class == 3:
-            _, seg0 = cv2.threshold(seg_label[:, :, 0], 128, 255, cv2.THRESH_BINARY)
-            _, seg1 = cv2.threshold(seg_label[:, :, 1], 1, 255, cv2.THRESH_BINARY)
-            _, seg2 = cv2.threshold(seg_label[:, :, 2], 1, 255, cv2.THRESH_BINARY)
-        else:
-            _, seg1 = cv2.threshold(seg_label, 1, 255, cv2.THRESH_BINARY)
-            _, seg2 = cv2.threshold(seg_label, 1, 255, cv2.THRESH_BINARY_INV)
-        _, lane1 = cv2.threshold(lane_label, 1, 255, cv2.THRESH_BINARY)
-        _, lane2 = cv2.threshold(lane_label, 1, 255, cv2.THRESH_BINARY_INV)
+        # if self.cfg.num_seg_class == 3:
+        #     _, seg0 = cv2.threshold(seg_label[:, :, 0], 128, 255, cv2.THRESH_BINARY)
+        #     _, seg1 = cv2.threshold(seg_label[:, :, 1], 1, 255, cv2.THRESH_BINARY)
+        #     _, seg2 = cv2.threshold(seg_label[:, :, 2], 1, 255, cv2.THRESH_BINARY)
+        # else:
+        #     _, seg1 = cv2.threshold(seg_label, 1, 255, cv2.THRESH_BINARY)
+        #     _, seg2 = cv2.threshold(seg_label, 1, 255, cv2.THRESH_BINARY_INV)
+        # _, lane1 = cv2.threshold(lane_label, 1, 255, cv2.THRESH_BINARY)
+        # _, lane2 = cv2.threshold(lane_label, 1, 255, cv2.THRESH_BINARY_INV)
         #        _,seg2 = cv2.threshold(seg_label[:,:,2],1,255,cv2.THRESH_BINARY)
         # # seg1[cutout_mask] = 0
         # # seg2[cutout_mask] = 0
 
         # seg_label /= 255
         # seg0 = self.Tensor(seg0)
-        if self.cfg.num_seg_class == 3:
-            seg0 = self.Tensor(seg0)
-        seg1 = self.Tensor(seg1)
-        seg2 = self.Tensor(seg2)
+        # if self.cfg.num_seg_class == 3:
+        #     seg0 = self.Tensor(seg0)
         # seg1 = self.Tensor(seg1)
         # seg2 = self.Tensor(seg2)
-        lane1 = self.Tensor(lane1)
-        lane2 = self.Tensor(lane2)
+        # seg1 = self.Tensor(seg1)
+        # seg2 = self.Tensor(seg2)
+        # lane1 = self.Tensor(lane1)
+        # lane2 = self.Tensor(lane2)
 
         # seg_label = torch.stack((seg2[0], seg1[0]),0)
-        if self.cfg.num_seg_class == 3:
-            seg_label = torch.stack((seg0[0], seg1[0], seg2[0]), 0)
-        else:
-            seg_label = torch.stack((seg2[0], seg1[0]), 0)
+        # if self.cfg.num_seg_class == 3:
+        #     seg_label = torch.stack((seg0[0], seg1[0], seg2[0]), 0)
+        # else:
+        #     seg_label = torch.stack((seg2[0], seg1[0]), 0)
 
-        lane_label = torch.stack((lane2[0], lane1[0]), 0)
+        # lane_label = torch.stack((lane2[0], lane1[0]), 0)
         # _, gt_mask = torch.max(seg_label, 0)
         # _ = show_seg_result(img, gt_mask, idx, 0, save_dir='debug', is_gt=True)
 
         target = [labels_out, seg_label, lane_label]
+        img = self.transform(img)
+
+        return img, target, data["image"], shapes
+
+    def __getitem__(self, idx):
+        """
+        Get input and groud-truth from database & add data augmentation on input
+
+        Inputs:
+        -idx: the index of image in self.db(database)(list)
+        self.db(list) [a,b,c,...]
+        a: (dictionary){'image':, 'information':}
+
+        Returns:
+        -image: transformed image, first passed the data augmentation in __getitem__ function(type:numpy), then apply self.transform
+        -target: ground truth(det_gt,seg_gt)
+
+        function maybe useful
+        cv2.imread
+        cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
+        cv2.warpAffine
+        """
+        data = self.db[idx]
+        img = cv2.imread(
+            data["image"], cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION
+        )
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        resized_shape = self.inputsize
+        if isinstance(resized_shape, list):
+            resized_shape = max(resized_shape)
+        h0, w0 = img.shape[:2]  # orig hw
+        r = resized_shape / max(h0, w0)  # resize image to img_size
+        if r != 1:  # always resize down, only resize up if training with augmentation
+            interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
+            img = cv2.resize(img, (int(w0 * r), int(h0 * r)), interpolation=interp)
+        h, w = img.shape[:2]
+
+        (img, _, _), ratio, pad = letterbox(
+            (img, img, img),
+            resized_shape,
+            auto=True,
+            scaleup=self.is_train,
+        )
+        shapes = (h0, w0), ((h / h0, w / w0), pad)  # for COCO mAP rescaling
+
+        det_label = data["label"]
+        labels = []
+
+        if det_label.size > 0:
+            # Normalized xywh to pixel xyxy format
+            labels = det_label.copy()
+            labels[:, 1] = (
+                ratio[0] * w * (det_label[:, 1] - det_label[:, 3] / 2) + pad[0]
+            )  # pad width
+            labels[:, 2] = (
+                ratio[1] * h * (det_label[:, 2] - det_label[:, 4] / 2) + pad[1]
+            )  # pad height
+            labels[:, 3] = (
+                ratio[0] * w * (det_label[:, 1] + det_label[:, 3] / 2) + pad[0]
+            )
+            labels[:, 4] = (
+                ratio[1] * h * (det_label[:, 2] + det_label[:, 4] / 2) + pad[1]
+            )
+
+        if len(labels):
+            # convert xyxy to xywh
+            labels[:, 1:5] = xyxy2xywh(labels[:, 1:5])
+
+            # Normalize coordinates 0 - 1
+            labels[:, [2, 4]] /= img.shape[0]  # height
+            labels[:, [1, 3]] /= img.shape[1]  # width
+
+        labels_out = torch.zeros((len(labels), 6))
+        if len(labels):
+            labels_out[:, 1:] = torch.from_numpy(labels)
+
+        img = np.ascontiguousarray(img)
+
+        target = labels_out
         img = self.transform(img)
 
         return img, target, data["image"], shapes
@@ -330,19 +410,14 @@ class KITTIDataset(Dataset):
     @staticmethod
     def collate_fn(batch):
         img, label, paths, shapes = zip(*batch)
-        label_det, label_seg, label_lane = [], [], []
-        for i, l in enumerate(label):
-            l_det, l_seg, l_lane = l
+        label_det= []
+        for i, l_det in enumerate(label):
             l_det[:, 0] = i  # add target image index for build_targets()
             label_det.append(l_det)
-            label_seg.append(l_seg)
-            label_lane.append(l_lane)
         return (
             torch.stack(img, 0),
             [
                 torch.cat(label_det, 0),
-                torch.stack(label_seg, 0),
-                torch.stack(label_lane, 0),
             ],
             paths,
             shapes,
--- tools/debug.py
+++ tools/debug.py
@@ -6,5 +6,15 @@ sys.path.append(BASE_DIR)
 
 from lib.config import cfg
 from lib.dataset.kitti import KITTIDataset
+import torchvision.transforms as transforms
 
-KITTIDataset(cfg, True)
+normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+
+dataset = KITTIDataset(cfg, True, transform=transforms.Compose(
+            [
+                transforms.ToTensor(),
+                normalize,
+            ]
+        ))
